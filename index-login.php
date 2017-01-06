2d1fb113fde7f63fd4dd722c1a9708b0362c08ce
CountCarrion <solairesbff@gmail.com>
2017-01-06 15:49:49 +0000

    added web files

---
+++ index-login.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+<body>
+Welcome <?php echo $_POST["username"]; ?><br>
+Your password address is: <?php echo $_POST["userpassword"]; ?><br>
+Your email address is: <?php echo $_POST["useremail"]; ?>
+</body>
+</html>
---
+++ user-profile.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<title>blogUmbra</title>
+<link href="blogumbra-style.css" rel="stylesheet" type="text/css">
+</head>
+
+
+<body>
+<div id="page_wrap">
+	
+</div>
+	
+	
+	<!--<script type="text/javascript" src="http://code.jquery.com/jquery-3.1.1.js"></script>-->
+	<script type="text/javascript" src="scripts/blogumbra-main.js"></script>
+</body>
+
+</html>
